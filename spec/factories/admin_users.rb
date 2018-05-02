86ec0221279a69fccb81f196b694a0a72b0b7897
Giovanni De Lucca <giovanni@platan.us>
2018-05-02 14:54:41 -0300

    chore(rubocop): remove rubocop's pending warnings

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :admin_user do
-    
+    sequence :email do |n|
+      "admin#{n}@example.com"
+    end
+    password "password123"
+    password_confirmation "password123"
   end
 end
