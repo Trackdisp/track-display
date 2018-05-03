33ceaba5deed77a0ef531e2ee4e668fe5a634768
Giovanni De Lucca <giovanni@platan.us>
2018-05-03 12:16:47 -0300

    feat(devise/sign_in): add initial view with header

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   mount Sidekiq::Web => '/queue'
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
