8ec35ac799126954781c8f1c74be30486f74c894
Giovanni De Lucca <giovanni@platan.us>
2018-05-23 13:45:49 -0400

    chore(policies): centralize policies and permit all actions

--- app/policies/campaign_policy.rb
+++ app/policies/campaign_policy.rb
@@ -1,25 +1,2 @@
 class CampaignPolicy < ApplicationPolicy
-  def index?
-    true
-  end
-
-  def show?
-    scope.where(id: record.id).exists?
-  end
-
-  def create?
-    index?
-  end
-
-  def update?
-    index?
-  end
-
-  def destroy?
-    index?
-  end
-
-  def destroy_all?
-    destroy?
-  end
 end
