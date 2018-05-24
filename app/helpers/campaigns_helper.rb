709f592fcdd0bf3e8f1f24ef590f04960c779b5b
Giovanni De Lucca <giovanni@platan.us>
2018-05-24 13:20:56 -0400

    feat(campaigns): add campaign show view

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -5,7 +5,9 @@ module CampaignsHelper
         { name: 'Total People', data: graphs_data[:total_people] },
         { name: 'Views Over 5', data: graphs_data[:views_over_5] }
       ],
-      legend: false, colors: %w(#11B0FC #00D976), library: { chart: { zoomType: 'x' } }
+      legend: false,
+      colors: %w(#11B0FC #00D976),
+      library: { chart: { zoomType: 'x' } }
     )
   end
 
