28be2eda0782bb7248339b46d90df26ec51f444e
Leandro Segovia <ldlsegovia@gmail.com>
2018-05-28 15:02:42 -0300

    fix(): job calling wrong method

--- app/jobs/es_index/update_campaign_measures_job.rb
+++ app/jobs/es_index/update_campaign_measures_job.rb
@@ -1,5 +1,5 @@
 class EsIndex::UpdateCampaignMeasuresJob < EsIndex::BaseJob
   def perform(campaign_id)
-    Elastic::IndexUpdater.update_device_measures(campaign_id)
+    Elastic::IndexUpdater.update_campaign_measures(campaign_id)
   end
 end
