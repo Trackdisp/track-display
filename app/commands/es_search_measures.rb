8e06ee315ad51b70f0724efbbf6ecfe2bd9271bf
Giovanni De Lucca <giovanni@platan.us>
2018-07-11 13:22:55 -0400

    feat(es_measures_search): add filter by location

--- app/commands/es_search_measures.rb
+++ app/commands/es_search_measures.rb
@@ -1,12 +1,24 @@
-class EsSearchMeasures < PowerTypes::Command.new(:campaign)
+class EsSearchMeasures < PowerTypes::Command.new(:campaign, location: nil)
   include Elasticsearch::DSL
   ES_SEARCH_SIZE = 10000
 
   def perform
-    definition = Search.new
-    definition.from = 0
-    definition.size = ES_SEARCH_SIZE
-    definition.query = Queries::Match.new campaign_name: @campaign.name
-    Measure.es_search definition
+    Measure.es_search query_definition
+  end
+
+  private
+
+  def query_definition
+    {
+      query: { bool: { must: check_campaing_and_location } },
+      from: 0,
+      size: ES_SEARCH_SIZE
+    }
+  end
+
+  def check_campaing_and_location
+    query = [{ term: { campaign_id: @campaign.id } }]
+    query.push(term: { location_id: @location.id }) if @location.present?
+    query
   end
 end
--- spec/commands/es_search_measures_spec.rb
+++ spec/commands/es_search_measures_spec.rb
@@ -5,12 +5,39 @@ describe EsSearchMeasures do
     described_class.for(*_args)
   end
 
+  let(:es_search_params) do
+    {
+      query: {
+        bool: {
+          must: [
+            term: { campaign_id: campaign.id }
+          ]
+        }
+      },
+      from: 0,
+      size: 10000
+    }
+  end
+
   describe '#perform' do
     let(:campaign) { create(:campaign) }
 
     it 'calls es_search on Measure correctly' do
-      expect(Measure).to receive(:es_search).and_return(nil)
+      expect(Measure).to receive(:es_search).with(es_search_params).and_return(nil)
       perform(campaign: campaign)
+    end
+
+    context 'with location params' do
+      let(:location) { create(:location) }
+
+      before do
+        es_search_params[:query][:bool][:must].push(term: { location_id: location.id })
+      end
+
+      it 'filters by location id' do
+        expect(Measure).to receive(:es_search).with(es_search_params).and_return(nil)
+        perform(campaign: campaign, location: location)
+      end
     end
   end
 end
