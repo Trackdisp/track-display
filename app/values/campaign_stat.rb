c32dafdef2f2302d5d46f61a36794ccca4d34f41
Giovanni De Lucca <giovanni@platan.us>
2018-07-26 13:22:14 -0400

    feat(obtain_campaign_stats): use calculate measures stats command

--- app/values/campaign_stat.rb
+++ app/values/campaign_stat.rb
@@ -1,18 +1,37 @@
 class CampaignStat
   include ActiveModel::SerializerSupport
   attr_accessor :campaign, :contacts_data, :contacts_sum, :total_data, :total_sum, :effectiveness,
-    :units_rotated_data, :units_rotated_sum
+    :units_rotated_data, :units_rotated_sum, :total_male, :total_female, :total_happiness,
+    :total_avg_age
 
   def initialize(opts = {})
     @campaign = opts[:campaign]
-    @contacts_data = opts[:contacts_data]
-    @contacts_sum = @contacts_data.values.sum
-    @total_data = opts[:total_data]
-    @total_sum = @total_data.values.sum
-    @units_rotated_data = opts[:units_rotated_data]
-    @units_rotated_sum = opts[:units_rotated_sum]
+
+    init_contacts_stats(opts[:contacts])
+    init_total_stats(opts[:total])
+    init_units_stats(opts[:units_rotated])
 
     calculate_effectiveness
+  end
+
+  def init_contacts_stats(opts = {})
+    @contacts_data = opts[:data]
+    @contacts_sum = opts[:sum]
+  end
+
+  def init_total_stats(opts = {})
+    @total_avg_age = opts[:avg_age]
+    @total_female = opts[:female]
+    @total_male = opts[:male]
+    @total_data = opts[:data]
+    @total_sum = opts[:sum]
+
+    @total_happiness = (opts[:happiness] * 100).round
+  end
+
+  def init_units_stats(opts = {})
+    @units_rotated_data = opts[:data]
+    @units_rotated_sum = opts[:sum]
   end
 
   def calculate_effectiveness
