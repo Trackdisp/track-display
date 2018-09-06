cc80471b42e73ca9d549abc4b50038317093090e
Diego Fernández <difernandez@uc.cl>
2018-09-06 16:03:18 -0300

    feat(admin-measures): add button to upload csv with measures

--- app/admin/measure.rb
+++ app/admin/measure.rb
@@ -2,6 +2,52 @@ ActiveAdmin.register Measure do
   permit_params :device_id, :measured_at, :avg_age, :w_id, :presence_duration, :contact_duration,
     :happiness, :gender
 
+  controller do
+    def upload_csv
+      if params[:csv].nil?
+        fail_upload(t("active_admin.measures.upload.empty"))
+      elsif !params[:csv].original_filename.end_with? ".csv"
+        fail_upload(t("active_admin.measures.upload.extension_error"))
+      else
+        save_measures(CSV.parse(params[:csv].read))
+        redirect_to admin_measures_path, notice: t("active_admin.measures.upload.success")
+      end
+    rescue ActiveRecord::RecordInvalid
+      fail_upload(t("active_admin.measures.upload.format_error"))
+    rescue
+      fail_upload(t("active_admin.measures.upload.general_error"))
+    end
+
+    private
+
+    def fail_upload(alert)
+      redirect_to admin_measures_path, alert: alert
+    end
+
+    def save_measures(csv_data)
+      csv_data[1..-1].each do |row|
+        Measure.create!(
+          device_id: row[0],
+          measured_at: row[1],
+          avg_age: row[2],
+          w_id: row[3],
+          presence_duration: row[4],
+          contact_duration: row[5],
+          happiness: row[6],
+          gender: row[7]
+        )
+      end
+    end
+  end
+
+  collection_action :upload, method: :get do
+    @page_title = t("active_admin.measures.upload.title")
+  end
+
+  action_item :upload, only: :index do
+    link_to t("active_admin.measures.upload.button"), upload_admin_measures_path
+  end
+
   filter :device
   filter :campaign
   filter :campaign_company_id, as: :select, collection: -> { Company.all }
