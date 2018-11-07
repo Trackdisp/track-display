60ddc7e68d317704cb1d1f44e71c921cabd74001
Diego Fernández <difernandez@uc.cl>
2018-11-07 17:09:40 -0300

    refactor(campaign-controller-spec): define filters in let in contexts

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -97,73 +97,77 @@ RSpec.describe CampaignsController, elasticsearch: true, type: :controller do
     end
 
     context "when multiple locations are selected" do
+      let(:locations) { [measures[0].location_id, measures[2].location_id] }
+
       it "assigns correct location values" do
-        get :show, params: { slug: campaign.slug, locations: [measures[0].location_id,
-                                                              measures[2].location_id] }
+        get :show, params: { slug: campaign.slug, locations: locations }
         expect(assigns(:selected_locations)).to match_array([measures[0].location,
                                                              measures[2].location])
       end
     end
 
     context "when multiple brands are selected" do
+      let(:brands) { [measures[0].location.brand_id, measures[2].location.brand_id] }
+
       it "sets location filter options considering only those of selected brands" do
-        get :show, params: { slug: campaign.slug, brands: [measures[0].location.brand_id,
-                                                           measures[2].location.brand_id] }
+        get :show, params: { slug: campaign.slug, brands: brands }
         expect(assigns(:locations)).to match_array([measures[0].location, measures[2].location])
       end
 
       it "assigns correct brand values" do
-        get :show, params: { slug: campaign.slug, brands: [measures[0].location.brand_id,
-                                                           measures[2].location.brand_id] }
+        get :show, params: { slug: campaign.slug, brands: brands }
         expect(assigns(:selected_brands)).to match_array([measures[0].location.brand,
                                                           measures[2].location.brand])
       end
     end
 
     context "when multiple channels are selected" do
+      let(:channels) { ["supermarket", "traditional"] }
+
       it "sets location filter options considering only those of selected channels" do
-        get :show, params: { slug: campaign.slug, channels: ["supermarket", "traditional"] }
+        get :show, params: { slug: campaign.slug, channels: channels }
         expect(assigns(:locations)).to match_array([measures[0].location, measures[2].location])
       end
 
       it "assigns correct channel values" do
-        get :show, params: { slug: campaign.slug, channels: ["supermarket", "traditional"] }
-        expect(assigns(:selected_channels)).to match_array(["supermarket", "traditional"])
+        get :show, params: { slug: campaign.slug, channels: channels }
+        expect(assigns(:selected_channels)).to match_array(channels)
       end
     end
 
     context "when multiple communes are selected" do
+      let(:communes) { [measures[0].location.commune_id, measures[2].location.commune_id] }
+
       it "sets location filter options considering only those of selected communes" do
-        get :show, params: { slug: campaign.slug, communes: [measures[0].location.commune_id,
-                                                             measures[2].location.commune_id] }
+        get :show, params: { slug: campaign.slug, communes: communes }
         expect(assigns(:locations)).to match_array([measures[0].location, measures[2].location])
       end
 
       it "assigns correct commune values" do
-        get :show, params: { slug: campaign.slug, communes: [measures[0].location.commune_id,
-                                                             measures[2].location.commune_id] }
+        get :show, params: { slug: campaign.slug, communes: communes }
         expect(assigns(:selected_communes)).to match_array([measures[0].location.commune,
                                                             measures[2].location.commune])
       end
     end
 
     context "when multiple regions are selected" do
+      let(:regions) do
+        [measures[0].location.commune.region_id, measures[2].location.commune.region_id]
+      end
+
       it "sets location filter options considering only those of selected regions" do
-        get :show, params: { slug: campaign.slug, regions:
-          [measures[0].location.commune.region_id, measures[2].location.commune.region_id] }
+        get :show, params: { slug: campaign.slug, regions: regions }
         expect(assigns(:locations)).to match_array([measures[0].location, measures[2].location])
       end
 
       it "sets commune filter options considering only those of selected regions" do
-        get :show, params: { slug: campaign.slug, regions:
-          [measures[0].location.commune.region_id, measures[2].location.commune.region_id] }
+        get :show, params: { slug: campaign.slug, regions: regions }
         expect(assigns(:communes)).to match_array([measures[0].location.commune,
                                                    measures[2].location.commune])
       end
 
       it "assigns correct region values" do
-        get :show, params: { slug: campaign.slug, regions:
-          [measures[0].location.commune.region_id, measures[2].location.commune.region_id] }
+        get :show, params: { slug: campaign.slug, regions: regions }
         expect(assigns(:selected_regions)).to match_array(
           [measures[0].location.commune.region, measures[2].location.commune.region]
         )
