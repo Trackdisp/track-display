c77a899b4dee5d0a8dc6d1ca30153b65b3ecb8f2
Diego Fernández <difernandez@uc.cl>
2018-11-13 14:25:45 -0300

    test(campaign-controller-api): add campaign_filter specs

--- spec/controllers/api/v1/campaigns_controller_spec.rb
+++ spec/controllers/api/v1/campaigns_controller_spec.rb
@@ -1,8 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CampaignsController, type: :controller do
+  let!(:campaign) { create(:campaign) }
+  let!(:other_campaign) { create(:campaign) }
+  let!(:measures) do
+    [
+      create(:measure,
+        device: create(:device,
+          active: true, location: create(:location, channel: "traditional"), campaign: campaign)),
+      create(:measure,
+        device: create(:device,
+          active: false, location: create(:location, channel: "local_consumption"),
+          campaign: campaign)),
+      create(:measure,
+        device: create(:device,
+          active: true, location: create(:location, channel: "supermarket"), campaign: campaign)),
+      create(:measure,
+        device: create(:device,
+          active: true, location: create(:location, channel: "local_consumption"),
+          campaign: campaign))
+    ]
+  end
+  let!(:other_meassures) do
+    [
+      create(:measure,
+        device: create(:device,
+          active: true, location: create(:location, channel: "local_consumption"),
+          campaign: other_campaign))
+    ]
+  end
+  let(:locations) { [measures[0].location_id, measures[2].location_id] }
+  let(:brands) { [measures[0].location.brand_id, measures[2].location.brand_id] }
+  let(:channels) { ["supermarket", "traditional"] }
+  let(:communes) { [measures[0].location.commune_id, measures[2].location.commune_id] }
+  let(:regions) do
+    [measures[0].location.commune.region_id, measures[2].location.commune.region_id]
+  end
+
   describe "GET #get_stats" do
-    let(:campaign) { create(:campaign) }
     let(:obtain_stats_response) do
       double(
         total_data: [],
@@ -17,7 +52,7 @@ RSpec.describe Api::V1::CampaignsController, type: :controller do
       {
         stats: [
           { name: I18n.t('messages.campaigns.people'), data: obtain_stats_response.total_data },
-          { name: I18n.t('messages.campaigns.contacts'), data: [] },
+          { name: I18n.t('messages.campaigns.contacts'), data: obtain_stats_response.total_data },
           {
             name: I18n.t('messages.campaigns.extracted'),
             data: obtain_stats_response.units_extracted_data,
@@ -27,11 +62,7 @@ RSpec.describe Api::V1::CampaignsController, type: :controller do
       }
     end
     before do
-      expect(ObtainCampaignStats).to receive(:for).and_return(obtain_stats_response)
-    end
-
-    it "calls ObtainCampaignStats command" do
-      get :get_stats, params: { slug: campaign.slug }
+      expect(subject).to receive(:obtain_campaign_stat).and_return(obtain_stats_response)
     end
 
     it "responds with ok status" do
@@ -39,9 +70,11 @@ RSpec.describe Api::V1::CampaignsController, type: :controller do
       expect(response).to have_http_status(:ok)
     end
 
-    it "returns formatted stats" do
+    it "renders formatted stats" do
       get :get_stats, params: { slug: campaign.slug }
       expect(response.body).to eq(formatted_stats.to_json)
     end
   end
+
+  it_should_behave_like(:campaign_filtered)
 end
