e989c635524a5b65cbc9b2bad6ce6c6f815325ce
Diego Fernández <difernandez@uc.cl>
2018-11-13 10:37:29 -0300

    test(campaign-controller-api): test response format

--- spec/controllers/api/v1/campaigns_controller_spec.rb
+++ spec/controllers/api/v1/campaigns_controller_spec.rb
@@ -13,6 +13,19 @@ RSpec.describe Api::V1::CampaignsController, type: :controller do
         avg_age_data: []
       )
     end
+    let(:formatted_stats) do
+      {
+        stats: [
+          { name: I18n.t('messages.campaigns.people'), data: obtain_stats_response.total_data },
+          { name: I18n.t('messages.campaigns.contacts'), data: [] },
+          {
+            name: I18n.t('messages.campaigns.extracted'),
+            data: obtain_stats_response.units_extracted_data,
+            yAxis: 1
+          }
+        ]
+      }
+    end
     before do
       expect(ObtainCampaignStats).to receive(:for).and_return(obtain_stats_response)
     end
@@ -24,6 +37,11 @@ RSpec.describe Api::V1::CampaignsController, type: :controller do
     it "responds with ok status" do
       get :get_stats, params: { slug: campaign.slug }
       expect(response).to have_http_status(:ok)
+    end
+
+    it "returns formatted stats" do
+      get :get_stats, params: { slug: campaign.slug }
+      expect(response.body).to eq(formatted_stats.to_json)
     end
   end
 end
