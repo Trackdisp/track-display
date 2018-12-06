6809b6a78919f67d79f39d873bcec66debe56d05
Diego Fernández <difernandez@uc.cl>
2018-12-06 12:49:33 -0300

    feat(sidebar): move it to vue component

---
+++ app/javascript/tools/sidebar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="campaign-details__sidebar">
+    <h3 class="campaign-details__title">{{ $t('messages.campaignDetails.filters.title') }}</h3>
+    <div class="campaign-details__filters-container">
+      <div class="campaign-details__filter">
+        <select-filter :options="locations" label="name" track-by="id" query-param="locations[]" :placeholder="$t('messages.campaignDetails.filters.locations')" :multiple="true"></select-filter>
+      </div>
+      <div class="campaign-details__filter">
+        <select-filter :options="brands" label="name" track-by="id" query-param="brands[]" :placeholder="$t('messages.campaignDetails.filters.brands')" :multiple="true"></select-filter>
+      </div>
+      <div class="campaign-details__filter">
+        <select-filter :options="channels" label="name" track-by="id" query-param="channels[]" :placeholder="$t('messages.campaignDetails.filters.channels')" :multiple="true"></select-filter>
+      </div>
+      <div class="campaign-details__filter-separator"></div>
+      <div class="campaign-details__filter">
+        <select-filter :options="communes" label="name" track-by="id" query-param="communes[]" :placeholder="$t('messages.campaignDetails.filters.communes')" :multiple="true"></select-filter>
+      </div>
+      <div class="campaign-details__filter">
+        <select-filter :options="regions" label="name" track-by="id" query-param="regions[]" :placeholder="$t('messages.campaignDetails.filters.regions')" :multiple="true"></select-filter>
+      </div>
+      <div class="campaign-details__filter-separator"></div>
+      <div class="campaign-details__filter">
+        <datetime-picker query-param="after" :placeholder="$t('messages.campaignDetails.filters.afterDate')"></datetime-picker>
+      </div>
+      <div class="campaign-details__filter">
+        <datetime-picker query-param="before" :placeholder="$t('messages.campaignDetails.filters.beforeDate')"></datetime-picker>
+      </div>
+      <div class="campaign-details__filter-separator"></div>
+      <div class="campaign-details__filter">
+        <select-filter :options="genders" label="name" track-by="id" query-param="gender" :placeholder="$t('messages.campaignDetails.filters.gender')"></select-filter>
+      </div>
+    </div>
+    <filter-button></filter-button>
+  </div>
+</template>
+
+<script>
+import SelectFilter from '../tools/select-filter.vue';
+import DatetimePicker from '../tools/datetime-filter';
+import FilterButton from '../tools/filter-button';
+
+export default {
+  props: ['locations', 'brands', 'channels', 'communes', 'regions', 'genders'],
+  components: {
+    SelectFilter,
+    DatetimePicker,
+    FilterButton,
+  },
+};
+</script>
