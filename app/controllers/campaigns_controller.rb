9f00ebdf652d9391809541b2d6035ea662d96b39
Diego Fernández <difernandez@uc.cl>
2018-12-11 13:30:15 -0300

    refactor(campaign-cont): move obtainment of filter options to own methods

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -16,31 +16,58 @@ class CampaignsController < BaseController
   private
 
   def set_filters_options
-    all_locs = all_campaign_locations
-
-    @locations = all_locs.filtered(build_locations_filters_hash)
-
-    @brands = Brand.where(id: all_locs.pluck(:brand_id).compact.uniq)
-    @channels = all_locs.reduce(Set.new) do |arr, loc|
-      arr.add(name: t("enumerize.channel.#{loc.channel}"), id: loc.channel) if loc.channel
-    end
-    @channels ||= []
-    @regions = Region.where(id: all_locs.map(&:region_id).uniq)
-    @communes = filtered_communes(all_locs)
+    location_options
+    brand_options
+    channel_options
+    region_options
+    commune_options
   end
 
   def all_campaign_locations
-    measure_locs_ids = campaign.measures.joins(:device).where
-                               .not(devices: { active: false },
-                                    measures: { location_id: nil })
-                               .pluck(:location_id).uniq
-    w_measure_locs_ids = campaign.weight_measures.joins(:device).where
+    if @all_locs
+      @all_locs
+    else
+      measure_locs_ids = campaign.measures.joins(:device).where
                                  .not(devices: { active: false },
-                                      weight_measures: { location_id: nil })
+                                      measures: { location_id: nil })
                                  .pluck(:location_id).uniq
-    all_locs_ids = measure_locs_ids | w_measure_locs_ids
+      w_measure_locs_ids = campaign.weight_measures.joins(:device).where
+                                   .not(devices: { active: false },
+                                        weight_measures: { location_id: nil })
+                                   .pluck(:location_id).uniq
+      all_locs_ids = measure_locs_ids | w_measure_locs_ids
 
-    all_locs_ids ? Location.where(id: all_locs_ids) : nil
+      @all_locs = all_locs_ids ? Location.where(id: all_locs_ids) : nil
+    end
+  end
+
+  def location_options
+    @locations ||= all_campaign_locations.filtered(build_locations_filters_hash)
+  end
+
+  def brand_options
+    @brands ||= Brand.where(id: all_campaign_locations.pluck(:brand_id).compact.uniq)
+  end
+
+  def channel_options
+    @channels ||= all_campaign_locations.reduce(Set.new) do |arr, loc|
+      arr.add(name: t("enumerize.channel.#{loc.channel}"), id: loc.channel) if loc.channel
+    end
+    @channels ||= []
+  end
+
+  def region_options
+    @regions ||= Region.where(id: all_campaign_locations.map(&:region_id).uniq)
+  end
+
+  def commune_options
+    all_communes_ids = all_campaign_locations.pluck(:commune_id).uniq
+    @communes ||= if @selected_regions.empty?
+                    Commune.where(id: all_communes_ids)
+                  else
+                    selected_regions_communes_ids = @selected_regions.map(&:communes).flatten.map(&:id)
+                    Commune.where(id: all_communes_ids & selected_regions_communes_ids)
+                  end
   end
 
   def build_locations_filters_hash
@@ -50,15 +77,5 @@ class CampaignsController < BaseController
     filters[:commune_ids] = @selected_communes.map(&:id) unless @selected_communes&.empty?
     filters[:region_ids] = @selected_regions.map(&:id) unless @selected_regions&.empty?
     filters
-  end
-
-  def filtered_communes(all_locs)
-    all_communes_ids = all_locs.pluck(:commune_id).uniq
-    if @selected_regions.empty?
-      Commune.where(id: all_communes_ids)
-    else
-      selected_regions_communes_ids = @selected_regions.map(&:communes).flatten.map(&:id)
-      Commune.where(id: all_communes_ids & selected_regions_communes_ids)
-    end
   end
 end
