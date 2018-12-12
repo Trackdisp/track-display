e5c0e049a8c6c1f70d0d8629d2b7e6eba050a7f1
Diego Fernández <difernandez@uc.cl>
2018-12-12 15:19:58 -0300

    refactor(campaign-cont.): move locations and communes options to concern

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -23,28 +23,6 @@ class CampaignsController < BaseController
     commune_options
   end
 
-  def all_campaign_locations
-    if @all_locs
-      @all_locs
-    else
-      measure_locs_ids = campaign.measures.joins(:device).where
-                                 .not(devices: { active: false },
-                                      measures: { location_id: nil })
-                                 .pluck(:location_id).uniq
-      w_measure_locs_ids = campaign.weight_measures.joins(:device).where
-                                   .not(devices: { active: false },
-                                        weight_measures: { location_id: nil })
-                                   .pluck(:location_id).uniq
-      all_locs_ids = measure_locs_ids | w_measure_locs_ids
-
-      @all_locs = all_locs_ids ? Location.where(id: all_locs_ids) : nil
-    end
-  end
-
-  def location_options
-    @locations ||= all_campaign_locations.filtered(build_locations_filters_hash)
-  end
-
   def brand_options
     @brands ||= Brand.where(id: all_campaign_locations.pluck(:brand_id).compact.uniq)
   end
@@ -58,24 +36,5 @@ class CampaignsController < BaseController
 
   def region_options
     @regions ||= Region.where(id: all_campaign_locations.map(&:region_id).uniq)
-  end
-
-  def commune_options
-    all_communes_ids = all_campaign_locations.pluck(:commune_id).uniq
-    @communes ||= if @selected_regions.empty?
-                    Commune.where(id: all_communes_ids)
-                  else
-                    selected_regions_communes_ids = @selected_regions.map(&:communes).flatten.map(&:id)
-                    Commune.where(id: all_communes_ids & selected_regions_communes_ids)
-                  end
-  end
-
-  def build_locations_filters_hash
-    filters = {}
-    filters[:brand_ids] = @selected_brands.map(&:id) unless @selected_brands&.empty?
-    filters[:channels] = @selected_channels unless @selected_channels&.empty?
-    filters[:commune_ids] = @selected_communes.map(&:id) unless @selected_communes&.empty?
-    filters[:region_ids] = @selected_regions.map(&:id) unless @selected_regions&.empty?
-    filters
   end
 end
