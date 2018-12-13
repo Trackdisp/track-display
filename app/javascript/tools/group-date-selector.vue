9408a673b0d89c63d4edd36b5a09c4be26f5ce22
Diego Fernández <difernandez@uc.cl>
2018-12-13 10:57:51 -0300

    feat(group-date-selector): disable buttons depending on day difference

--- app/javascript/tools/group-date-selector.vue
+++ app/javascript/tools/group-date-selector.vue
@@ -3,10 +3,10 @@
     <v-btn flat value="week">
       {{ $t('groupDateSelector.week')}}
     </v-btn>
-    <v-btn flat value="day">
+    <v-btn flat value="day" :disabled="shouldDisableDay">
       {{ $t('groupDateSelector.day')}}
     </v-btn>
-    <v-btn flat value="hour">
+    <v-btn flat value="hour" :disabled="shouldDisableHour">
       {{ $t('groupDateSelector.hour')}}
     </v-btn>
   </v-btn-toggle>
@@ -15,12 +15,26 @@
 <script>
 import { changeURLQueryParam } from '../helpers/url-helper';
 
+const HOUR_LIMIT = 4;
+const DAY_LIMIT = 30;
+
 export default {
   props: ['initialGroup'],
   data() {
     return {
       groupBy: this.initialGroup,
     };
+  },
+  computed: {
+    chartDatesDiffInDays() {
+      return this.$store.getters.chartDatesDiffInDays;
+    },
+    shouldDisableHour() {
+      return this.chartDatesDiffInDays > HOUR_LIMIT;
+    },
+    shouldDisableDay() {
+      return this.chartDatesDiffInDays > DAY_LIMIT;
+    },
   },
   watch: {
     groupBy(val) {
