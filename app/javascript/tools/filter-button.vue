f69f43b08d4b84cbc40009846dad63cd9ba74dd9
Diego Fernández <difernandez@uc.cl>
2018-12-26 16:08:52 -0300

    feat(filter-button): add disabled behavior to apply button

--- app/javascript/tools/filter-button.vue
+++ app/javascript/tools/filter-button.vue
@@ -4,8 +4,11 @@
       <img src="~/close-red.svg">
       Limpiar
     </div>
-    <div class="filter-button__button" @click="applyFilters">
-      <img src="~/check.svg">
+    <div class="filter-button__button"
+         :class="{'filter-button__button--disabled': !selectedFiltersChanged}"
+         @click="applyFilters">
+      <img src="~/check.svg" v-if="selectedFiltersChanged">
+      <img src="~/check-disabled.svg" v-else>
       Aplicar
     </div>
   </div>
@@ -18,10 +21,15 @@ export default {
     filtersQueryString() {
       return this.$store.getters.filtersQueryString();
     },
+    selectedFiltersChanged() {
+      return this.$store.getters.selectedFiltersChanged;
+    },
   },
   methods: {
     applyFilters() {
-      window.location.search = this.filtersQueryString;
+      if (this.selectedFiltersChanged) {
+        window.location.search = this.filtersQueryString;
+      }
     },
     cleanFilters() {
       this.$store.dispatch('cleanFilters');
